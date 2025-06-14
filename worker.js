1e0da6de11b4ad7254255f3fe3c6eb1cf0796778
Simon <github@lazybirbo.de>
2025-06-14 13:42:57 +0200

    Update worker.js
    This is the current LIVE version, its probably not the best way to do it, but i think its quite alright like this

--- worker.js
+++ worker.js
@@ -1,15 +1,41 @@
 export default {
   async fetch(request, env) {
     
-    const { search, pathname } = new URL(request.url);
+    const { search, pathname, hostname } = new URL(request.url);
 
     var steamURL = "steam:/" + pathname;
 
-    if (steamURL === "steam:/")
-    {
-      steamURL = "steam://";
-    }
+    const help = `
+    <html>
+    <head>
+    <title>Help page!!</title>
+    </head>
+    <body>
+    <p>Welcome to the Steam Protocol Redirector, a API that makes the "steam://" protocol easier to share by having a HTTP(S) API that redirects to that protocol.
+    <br>
+    <br>
+    <br>
+    The API is held simple, just replace the "steam://" with the domain. 
+    <br>
+    <br>
+    Example:
+    <br>
+    steam://install/440 --> ${hostname}/install/440
+    <br>
+    <br>
+    <br>
+    The API responds with a 302 temporary redirect, your browser will then ask to open Steam.<br><br>LazyBirbo @ 2025
+    <br>E-Mail: <a href="mailto:github@lazybirbo.de">github@lazybirbo.de</a><br>GitHub Repository: <a href="https://github.com/LazyBirb/steam-redirector" target="_blank" rel="noopener noreferrer">https://github.com/LazyBirb/steam-redirector</a>
+    </p>
+    </body>
+    </html>
+    `
 
+
+    if (pathname === "/")
+    {
+      return new Response(help,{headers: {"content-type": "text/html;charset=UTF-8","bird-type": "LazyBirbo"}});
+    }
 
     return Response.redirect(steamURL,302);
     
